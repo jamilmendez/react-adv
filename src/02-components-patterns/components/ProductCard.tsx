f6a7c4f960c9bb9b039add3a6332076953f72d14
jamilmendez <mdztnr99@gmail.com>
2022-02-03 17:45:01 -0500

    acomponents Ãpatterns added

---
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -0,0 +1,38 @@
+import styles from '../styles/styles.module.css';
+import noImage from '../assets/no-image.jpg';
+import { useProduct } from '../hooks/useProduct';
+
+interface Props {
+    product: Product;
+}
+
+interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export const ProductCard = ({ product }:Props) => {
+
+    const {counter, increaseBy } = useProduct();
+
+    
+
+    return (
+        <div className={ styles.productCard}>
+            <img className={ styles.productImg } src={ product.img ? product.img : noImage } alt="Coffee Mug" />
+
+            <span className={ styles.productDescription }>{ product.title }</span>
+            
+            <div className={ styles.buttonsContainer }>
+                <button
+                    className={ styles.buttonMinus }
+                    onClick={ () => increaseBy(-1) }> - </button>
+                <div className={ styles.countLabel }>{ counter }</div>
+                <button 
+                    className={ styles.buttonAdd } 
+                    onClick={ () => increaseBy(1) }> + </button>
+            </div>
+        </div>
+    )
+};

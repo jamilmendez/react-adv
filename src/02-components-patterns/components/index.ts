b954dceaa52e62731b2f1067df9888a17fd34cdc
jamilmendez <mdztnr99@gmail.com>
2022-02-04 16:48:16 -0500

    component extends styles ready

--- src/02-components-patterns/components/index.ts
+++ src/02-components-patterns/components/index.ts
@@ -9,7 +9,7 @@ export { ProductButtons } from "./ProductButtons";
 export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 
-export const ProductCard: ProductCardHOCProps = Object.assign( ProductCardHOC, {
+export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC, {
     Title: ProductTitle,
     Image: ProductImage,
     Buttons: ProductButtons
